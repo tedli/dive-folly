1d486e1ce74bfd1f4880e41a973a90065a4808fe
lizhen <lizhen@outlook.jp>
2023-10-21 21:54:43 +0800

    init

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char *argv[]) {
   folly::ConcurrentHashMap<int, int> a;
+  std::cout << "a.empty(): " << a.empty() << std::endl;
   std::cout << "Hello, World!" << std::endl;
   return 0;
 }
