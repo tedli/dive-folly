9f20e074932e202699d1bb4fc4cc7a8593ae9d78
lizhen <lizhen@outlook.jp>
2023-10-23 18:33:06 +0800

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <string>
 
 #include <folly/concurrency/ConcurrentHashMap.h>
 
 int main(int argc, char *argv[]) {
-  folly::ConcurrentHashMap<int, int> a;
+  folly::ConcurrentHashMap<std::string, int> a;
+  a.insert_or_assign("one", 1);
+  a.insert_or_assign("two", 2);
+
+  for (auto i : a) {
+    std::cout << "key: " << i.first << ", value: " << i.second << std::endl;
+  }
+
+  std::cout << "a.find(\"three\") == a.cend(): " << (a.find("three") == a.cend()) << std::endl;
   std::cout << "a.empty(): " << a.empty() << std::endl;
   std::cout << "Hello, World!" << std::endl;
   return 0;
